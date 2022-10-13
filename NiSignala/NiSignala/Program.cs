568e6e2cd2d8ecac666a82bd1ffb089a5a2930de
Alojz Bajc <alojz.bajc@scng.si>
2022-10-13 10:13:56 +0200

    Mobitel

---
+++ NiSignala/NiSignala/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kupec janez = new kupec();
+            janez.Ime = ("Janez Novak");
+            janez.BeležiKlic(10, TipKlica.Stacionarno);
+            Console.WriteLine(janez.Ime + " dolguje " + janez.Stanje);
+
+            kupec alenka = new kupec();
+            alenka.Ime = ("Alenka Novak");
+            alenka.BeležiKlic(10, TipKlica.Mobilno);
+            Console.WriteLine(alenka.Ime + " dolguje " + alenka.Stanje);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ NiSignala/NiSignala/kupec.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class kupec
+    {
+        private string ime;
+        private decimal stanje;
+
+        public string Ime { get => ime; set => ime = value; }
+        public decimal Stanje { get => stanje; }
+        //Ne moreš spreminjat   
+        public void BeležiPlačilo(decimal plačilo)
+
+        {
+            stanje -= plačilo;
+        }
+
+        public void BeležiKlic(int min, TipKlica tip)
+        {
+            switch (tip)
+            {
+                case TipKlica.Stacionarno:
+                    stanje += min * 0.12m;
+
+                    break;
+
+                case TipKlica.Mobilno:
+                    stanje += min * 0.3m;
+
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+
+}
